5f5bd3b913335ca8f11ebaced650e9786b16659f
justCupOfTea <91449444+justcupoftea@users.noreply.github.com>
2023-07-08 09:17:08 +0300

    while 10

--- src/org/problembook/_while/Main.java
+++ src/org/problembook/_while/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("while1: " + While1.fun(2, 1.0));
         System.out.println("while1: " + While1.fun(1.1, 1));
+
+        System.out.println("while10: " + While10.fun(2));
+        System.out.println("while10: " + While10.fun(8));
+
     }
 }
