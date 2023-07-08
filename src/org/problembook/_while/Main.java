e42688dc459576124cbe455604d795652316e842
Oleg <noemail@org>
2023-07-08 08:38:34 +0300

    while 1

--- src/org/problembook/_while/Main.java
+++ src/org/problembook/_while/Main.java
@@ -2,6 +2,7 @@ package org.problembook._while;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("while1: " + While1.fun(2, 1.0));
+        System.out.println("while1: " + While1.fun(1.1, 1));
     }
 }
---
+++ src/org/problembook/_while/While1.java
@@ -0,0 +1,17 @@
+package org.problembook._while;
+
+public class While1 {
+    static double fun(double a, double b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("A,B > 0");
+        }
+        if (a < b) {
+            throw new IllegalArgumentException("A<B");
+        }
+
+        while (a >= b) {
+            a -= b;
+        }
+        return a;
+    }
+}
