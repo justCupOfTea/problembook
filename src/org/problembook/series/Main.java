a3421cebab4eaef376dd63eeea9eea1bf313da6b
Oleg <noemal@org.ru>
2023-07-15 08:41:26 +0300

    Series28

--- src/org/problembook/series/Main.java
+++ src/org/problembook/series/Main.java
@@ -2,6 +2,7 @@ package org.problembook.series;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Series23.fun(5));
+//        System.out.println(Series23.fun(5));
+        Series28.fun(6);
     }
 }
