44d92de11f89c3b42bf86e1bc50d6ad9f0014afb
Oleg <noemal@org.ru>
2023-07-15 10:05:41 +0300

    Minmax10

--- src/org/problembook/minmax/Main.java
+++ src/org/problembook/minmax/Main.java
@@ -2,6 +2,7 @@ package org.problembook.minmax;
 
 public class Main {
     public static void main(String[] args) {
-        Minmax1.fun(3);
+//        Minmax1.fun(3);
+        Minmax10.fun(3);
     }
 }
