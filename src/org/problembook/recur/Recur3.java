f7ff933ece8b68af7e38a721bfd8972ba1f62727
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2023-07-29 14:01:34 +0300

    requr 1,2

---
+++ src/org/problembook/recur/Recur3.java
@@ -0,0 +1,13 @@
+package org.problembook.recur;
+
+public class Recur3 {
+
+    public static void print(int n) {
+        System.out.println("Recur3: " + fun(n));
+    }
+
+    private static int fun(int n) {
+
+        return ;
+    }
+}
