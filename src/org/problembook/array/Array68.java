81acc13e9e17cd740b43b147e06c29f144f68d3f
Oleg <noemal@org.ru>
2023-07-29 10:51:48 +0300

    Array68

---
+++ src/org/problembook/array/Array68.java
@@ -0,0 +1,30 @@
+package org.problembook.array;
+
+import java.util.Arrays;
+
+public class Array68 {
+    public static void fun(int n, double[] a) {
+        if (n < 1) {
+            throw new IllegalArgumentException("N > 0");
+        }
+
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+        int iMin = 0;
+        int iMax = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] > max) {
+                iMax = i;
+                max = a[i];
+            }
+            if (a[i] < min) {
+                iMin = i;
+                min = a[i];
+            }
+        }
+        a[iMax] = min;
+        a[iMin] = max;
+
+        System.out.println("Array68: " + Arrays.toString(a));
+    }
+}
