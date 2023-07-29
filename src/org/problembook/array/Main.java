fe5224865de0fa8672b4a1f10291a711ce01fba4
justCupOfTea <91449444+justcupoftea@users.noreply.github.com>
2023-07-29 10:51:48 +0300

    Array68

--- src/org/problembook/array/Main.java
+++ src/org/problembook/array/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Array3.fun(4,1,2);
         Array65.fun(5, new double[]{1,3,2,4,5},3);
+        Array68.fun(5, new double[]{2,3,-1,7,-1});
     }
 }
