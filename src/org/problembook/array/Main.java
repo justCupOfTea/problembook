45858046dcc2c3332f4d10e61b85c4175a8f20d8
justCupOfTea <91449444+justcupoftea@users.noreply.github.com>
2023-07-29 10:32:34 +0300

    Array65

--- src/org/problembook/array/Main.java
+++ src/org/problembook/array/Main.java
@@ -3,5 +3,6 @@ package org.problembook.array;
 public class Main {
     public static void main(String[] args) {
         Array3.fun(4,1,2);
+        Array65.fun(4, new double[]{1,3,2,4,5},3);
     }
 }
