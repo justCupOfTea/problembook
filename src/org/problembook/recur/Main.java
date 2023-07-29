e387eabbd733388fb655c1f80a4edfb1d925e871
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2023-07-29 14:33:11 +0300

    requr 3

--- src/org/problembook/recur/Main.java
+++ src/org/problembook/recur/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Recur1.print(5);
         Recur2.print(9);
+        Recur3.print(5, 2);
+        Recur3.print(-5, 2);
     }
 }
--- src/org/problembook/recur/Recur3.java
+++ src/org/problembook/recur/Recur3.java
@@ -2,12 +2,26 @@ package org.problembook.recur;
 
 public class Recur3 {
 
-    public static void print(int n) {
-        System.out.println("Recur3: " + fun(n));
+    public static void print(int n, double x) {
+        System.out.println("Recur3: " + fun(n, x));
     }
 
-    private static int fun(int n) {
+    private static double fun(int n, double x) {
+        if (x == 0) {
+            throw new IllegalArgumentException("x!=0");
+        }
+        if (n == 0) {
+            return 1;
+        } else if (n > 0 && n % 2 == 0) {
+            double v = fun(n / 2, x);
+            return v * v;
+        } else if (n > 0 && n % 2 != 0) {
+            return x * fun(n - 1, x);
+        } else if (n < 0) {
+            return 1 / fun(-n, x);
+        } else {
+            throw new IllegalStateException("error");
+        }
 
-        return ;
     }
 }
